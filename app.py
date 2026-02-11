38451217c26c65462e17e6ac710d71ddab6f0dea
Hardik <modihardik19@gmail.com>
2026-02-11 18:39:20 +0530

    Added health endpoint

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Welcome to PR POC Demo"
+
+@app.route("/health")
+def health():
+    return jsonify({"status": "UP"})
 
 if __name__ == "__main__":
     app.run(debug=True)
