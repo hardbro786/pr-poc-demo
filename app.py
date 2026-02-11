094b59098e295775e420cb65e0d980a10fe57dba
Hardik <modihardik19@gmail.com>
2026-02-11 17:46:17 +0530

    Feature: updated

--- app.py
+++ app.py
@@ -1,14 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return "Welcome to PR POC Demo"
-
-@app.route("/health")
-def health():
-    return jsonify({"status": "UP"})
 
 if __name__ == "__main__":
     app.run(debug=True)
